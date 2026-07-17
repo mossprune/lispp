6e3bdef2b23737504562c86e0733dc2204f091d9
moss <250817288+mossprune@users.noreply.github.com>
2026-07-17 09:42:39 +0530

    init repl

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
+
 #include <stdio.h>
 
+static char input[2048];
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+	puts("lispyy v.0.1.0");
+	puts("Press ctrl + c to exit");
+
+	while (1) {
+		fputs("lispyy >> ", stdout);
+		fgets(input, 2048, stdin);
+		printf("No you're a %s", input);
+	}
+
     return 0;
 }
